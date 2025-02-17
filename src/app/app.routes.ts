8ec406d3b0bdadc610f2b9e3c419c8cdb04b6b53
HectorCastillo <hector.delagarza@serviciosexternos.com.mx>
2025-02-17 16:07:23 -0600

    git commit -m "Initial commit for Angular portfolio"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,12 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './Modules/components/home/home.component';
+import { ProjectsComponent } from './Modules/components/projects/projects.component';
+import { ContactComponent } from './Modules/components/contact/contact.component';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [  
+  { path: '', component: HomeComponent },
+  { path: 'projects', component: ProjectsComponent },
+  { path: 'contact', component: ContactComponent }
+];
